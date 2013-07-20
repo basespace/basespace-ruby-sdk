57ca720887cdab347dedf21b65d2b799a96853d4
Joachim Baran <joachim.baran@gmail.com>
2013-07-20 12:41:15 -0400

    Brought multipart upload closer to a Ruby implementation, but still incomplete (i.e., non-functional).

--- lib/basespace/model/multipart_upload.rb
+++ lib/basespace/model/multipart_upload.rb
@@ -31,7 +31,7 @@ class UploadTask
   # +total+:: Total number of parts in the multi-part upload.
   # +myfile+:: Local file to be uploaded.
   # +attempt+:: Number of attempts that the file was previously uploaded (upload tries).
-  def initialize(self, api, bs_file_id, part, total, myfile, attempt)
+  def initialize(api, bs_file_id, part, total, myfile, attempt)
     @api         = api
     @part        = part       # part number
     @total       = total       # out of total part count
@@ -75,12 +75,17 @@ class UploadTask
   end
 
 end # class UploadTask
-    
-class Consumer  # [TODO] inherit multiprocessing.Process
+
+# Multipart file upload consumer class.
+#
+# TODO This file is not yet ported as the multipartFileUpload class is
+# just mentioned in the comment section of the BaseSpaceAPI file.
+class Consumer
+
   def initialize(task_queue, result_queue, pause_event, halt_event)
-    # [TODO] http://stackoverflow.com/questions/710785/working-with-multiple-processes-in-ruby
-    #        http://stackoverflow.com/questions/855805/please-introduce-a-multi-processing-library-in-perl-or-ruby
-    #        http://docs.python.jp/2.6/library/multiprocessing.html
+    # TODO http://stackoverflow.com/questions/710785/working-with-multiple-processes-in-ruby
+    #      http://stackoverflow.com/questions/855805/please-introduce-a-multi-processing-library-in-perl-or-ruby
+    #      http://docs.python.jp/2.6/library/multiprocessing.html
     #multiprocessing.Process.__init__(self)
     @task_queue    = task_queue
     @result_queue  = result_queue
@@ -88,43 +93,46 @@ class Consumer  # [TODO] inherit multiprocessing.Process
     @halt          = haltEvent
   end
   
-  # [TODO]
+  # TODO
   def run
     proc_name = self.name
-    while True:
+    while True
       unless self.pause.is_set()
         next_task = self.task_queue.get()
       end
             
-      if next_task is None or self.halt.is_set(): # check if we are out of jobs or have been halted
+      if next_task is None or self.halt.is_set() # check if we are out of jobs or have been halted
         # Poison pill means shutdown
-        print '%s: Exiting' % proc_name
+        puts "#{proc_name}: Exiting"
         self.task_queue.task_done()
         break
-      elsif self.pause.is_set():                   # if we have been paused, sleep for a bit then check back
-        print '%s: Paused' % proc_name 
+      elsif self.pause.is_set()                   # if we have been paused, sleep for a bit then check back
+        puts "#{proc_name}: Paused"
         time.sleep(3)                                       
-      else:                                       # do some work
-        print '%s: %s' % (proc_name, next_task)
+      else                                       # do some work
+        puts "#{proc_name}: #{next_task}"
         answer = next_task()
         self.task_queue.task_done()
-        if answer.state == 1:                   # case everything went well
+        if answer.state == 1                   # case everything went well
           self.result_queue.put(answer)
-        else:                                   # case something sent wrong
-          if next_task.attempt < 3:
+        else                                   # case something sent wrong
+          if next_task.attempt < 3
             self.task_queue.put(next_task)  # queue the guy for a retry
-          else:                               # problems, shutting down this party
+          else                               # problems, shutting down this party
             self.halt.set()                 # halt all other process
           end
         end
       end
     end
-    return
   end
 end # class Consumer
 
+# Multipart file upload class.
+#
+# TODO This file is not yet ported as the multipartFileUpload class is
+# just mentioned in the comment section of the BaseSpaceAPI file.
+class MultipartUpload
 
-class MultipartUpload:
   def initialize(api, a_id, local_file, file_object, cpu_count, part_size, temp_dir, verbose)
     @api            = api
     @analysis_id    = a_id
@@ -141,13 +149,14 @@ class MultipartUpload:
   end
 
   def to_s
-    # [TODO] fix this.
+    # TODO fix this.
+    # NOTE Python sublist notation [:5] already changed to Ruby [0..4]
     return "MPU -  Stat: " + @status +  ", LiveThread: " + str(self.getRunningThreadCount()) + \
-                ", RunTime: " + str(self.getRunningTime())[:5] + 's' + \
+                ", RunTime: " + str(self.getRunningTime())[0..4] + 's' + \
                 ", Q-size " + str(self.tasks.qsize()) + \
                 ", Completed " + str(self.getProgressRatio()) + \
                 ", AVG TransferRate " + self.getTransRate() + \
-                ", Data transfered " + str(self.getTotalTransfered())[:5] + 'Gb'
+                ", Data transfered " + str(self.getTotalTransfered())[0..4] + 'Gb'
   end
     
   def to_str
@@ -165,7 +174,7 @@ class MultipartUpload:
     totalSize = os.path.getsize(self.localFile)
     fileCount = int(math.ceil(totalSize/(self.partSize*1024.0*1000)))
     
-    if self.verbose: 
+    if self.verbose 
       print "TotalSize " + str(totalSize)
       print "Using split size " + str(self.partSize) +"Mb"
       print "Filecount " + str(fileCount)
@@ -175,110 +184,118 @@ class MultipartUpload:
     # Establish communication queues
     self.tasks = multiprocessing.JoinableQueue()
     self.completedPool = multiprocessing.Queue()
-    for i in xrange(1,fileCount+1):         # set up the task queue
-      t = uploadTask(self.api,self.remoteFile.Id,i, fileCount, self.localFile, 0)
+    [1..fileCount].each { |i|         # set up the task queue
+      t = uploadTask(self.api,self.remoteFile.Id, i, fileCount, self.localFile, 0)
       self.tasks.put(t)
-    end
+    }
     self.totalTask  = self.tasks.qsize()
     
     # create consumers
     self.pauseEvent = multiprocessing.Event()
     self.haltEvent = multiprocessing.Event()
-    if self.verbose:
+    if self.verbose
       print 'Creating %d consumers' % self.cpuCount
       print "queue size " + str(self.tasks.qsize())
     end
-    self.consumers = [ Consumer(self.tasks, self.completedPool,self.pauseEvent,self.haltEvent) for i in xrange(self.cpuCount) ]
-      for c in self.consumers: self.tasks.put(None)   # add poisson pill
-    end
+    # NOTE Original code -- note the strange indent. Variables i and c not used. Buggy code?
+    # self.consumers = [ Consumer(self.tasks, self.completedPool,self.pauseEvent,self.haltEvent) for i in xrange(self.cpuCount) ]
+    #   for c in self.consumers: self.tasks.put(None)   # add poisson pill
+    self.consumers = [0..self.cpuCount].map { |i| Consumer(self.tasks, self.completedPool, self.pauseEvent, self.haltEvent) }
+    self.consumers.each { |c| self.tasks.put(nil) }
   end
         
-  def __cleanUp__(self):
+  def __cleanUp__
     self.stats[0] += 1
   end
     
-  def startUpload(self,returnOnFinish=0,testInterval=5):
-    if self.Status=='Terminated' or self.Status=='Completed':
-      raise Exception('Cannot resume a ' + self.Status + ' multi-part upload session.')
+  def startUpload(returnOnFinish = 0, testInterval = 5)
+    raise Exception('Cannot resume a ' + self.Status + ' multi-part upload session.') if self.Status == 'Terminated' or self.Status == 'Completed'
         
-    if self.Status == 'Initialized':
+    if self.Status == 'Initialized'
       self.StartTime = time.time()
-      for w in self.consumers:
+      self.consumers.each { |w|
         w.start()
-    if self.Status == 'Paused':
+      }
+    elsif self.Status == 'Paused'
       self.pauseEvent.clear()
+    end
     self.Status = 'Running'
         
     # If returnOnFinish is set 
-    if returnOnFinish:
+    if returnOnFinish
       i=0
-      while not self.hasFinished():
-        if self.verbose and i: print str(i) + ': ' + str(self)
+      while not self.hasFinished()
+        print str(i) + ': ' + str(self) if self.verbose and i
         time.sleep(testInterval)
         i+=1
+      end
       self.finalize()
       return 1
-    else:
+    else
       return 1
     end
   end
     
-  def finalize(self):
-    if self.getRunningThreadCount():
-      raise Exception('Cannot finalize a transfer with running threads.')
-    if self.Status=='Running':
+  def finalize
+    raise Exception('Cannot finalize a transfer with running threads.') if self.getRunningThreadCount()
+    if self.Status=='Running'
       # code here for 
       self.Status=='Completed'
-    else:
+    else
       raise Exception('To finalize the status of the transfer must be "Running."')
     end
   end
     
-  def hasFinished(self):
-    if self.Status == 'Initialized': return 0
-    return not self.getRunningThreadCount()>0
+  def hasFinished
+    return 0 if self.Status == 'Initialized'
+    return !(self.getRunningThreadCount() > 0)
   end
     
-  def pauseUpload(self):
+  def pauseUpload
     self.pauseEvent.set()
     self.Status = 'Paused'
   end
     
-  def haltUpload(self):
-    for c in self.consumers: c.terminate()
+  def haltUpload
+    self.consumers.each { |c| c.terminate() }
     self.Status = 'Terminated'
   end
     
-  def getStatus(self):
+  def getStatus
     return self.Status
   end
     
-  def getFileResponse(self):
+  def getFileResponse
     return self.remoteFile
   end
     
-  def getRunningThreadCount(self):
-    return sum([c.is_alive() for c in self.consumers])
+  def getRunningThreadCount
+    return sum(self.consumers.map { |c| c.is_alive() })
   end
 
-  def getTransRate(self):
+  def getTransRate
     # tasks completed                        size of file-parts 
-    return str((self.totalTask - self.tasks.qsize())*self.partSize/self.getRunningTime())[:6] + ' mb/s'
+    # NOTE Python sublist [:6] already ported to Ruby [0..5]
+    return str((self.totalTask - self.tasks.qsize())*self.partSize/self.getRunningTime())[0..5] + ' mb/s'
   end
     
-  def getRunningTime(self):
-    if self.StartTime==-1: return 0
-    else: return time.time() - self.StartTime
+  def getRunningTime
+    if self.StartTime==-1
+      return 0
+    else
+      return time.time() - self.StartTime
+    end
   end
   
   # Returns the total data amoun transfered in Gb
-  def getTotalTransfered(self):
-    return float((self.totalTask - self.tasks.qsize())*self.partSize)/1000.0
+  def getTotalTransfered
+    return float((self.totalTask - self.tasks.qsize())*self.partSize) / 1000.0
   end
     
-  def getProgressRatio(self):
+  def getProgressRatio
     currentQ = float(self.tasks.qsize() - len(self.consumers))
-    return str(float(self.totalTask - currentQ)/self.totalTask)[:6]
+    # NOTE Python sublist [:6] already ported to Ruby [0..5]
+    return str(float(self.totalTask - currentQ) / self.totalTask)[0..5]
   end
 
 end # class MultipartUpload
